c1df19987daa57c89848a598769fe44503b928fc
Paz Dangur <pazdangur1@gmail.com>
2015-06-06 18:59:18 +0300

    yago parser

--- src/il/ac/tau/cs/databases/atlas/db/YagoParser.java
+++ src/il/ac/tau/cs/databases/atlas/db/YagoParser.java
@@ -12,9 +12,18 @@ public class YagoParser {
     public static final String GEO_INFO_OUT_NAME = "geo_info.tsv";
     public static final String WIKI_INFO_OUT_NAME = "wiki_info.tsv";
     public static final String CATEGORIES_INFO_OUT_NAME = "categories_info.tsv";
+    public static final String DATE_BORN_ON_DATE_OUT_NAME = "date_bornOnDate.tsv";
+    public static final String DATE_DIED_ON_DATE_OUT_NAME = "date_diedOnDate.tsv";
+    public static final String FACTS_GENDER_OUT_NAME = "facts_gender.tsv";
+    public static final String FACTS_BORN_IN_LOCATION_OUT_NAME = "facts_born_in_location.tsv";
+    public static final String FACTS_DIED_IN_LOCATION_OUT_NAME = "facts_died_in_location.tsv";
+    public static final String LABELS_INFO_OUT_NAME = "labels_info.tsv";
+    public static final String PREF_LABELS_INFO_OUT_NAME = "pref_labels_info.tsv";
+
     public static final String GEONAMES_URL_REGEX = "http://sws.geonames.org/([0-9]+)";
     public static final String DATE_REGEX = "[0-9][0-9][0-9][0-9]-[#0-9][#0-9]-[#0-9][#0-9]";
     public static final String CATEGORY_REGEX = "<wordnet_(.+)_[0-9]+>";
+    public static final String LABEL_REGEX = "\"(.*)\"((@eng)?)";
 
     private final File yagoDateFile;
     private final File yagoLocationFile;
@@ -24,11 +33,8 @@ public class YagoParser {
     private final File yagoGeonamesFile;
     private final File geonamesCitiesFile;
     private final String outputPath;
-    private Set<String> dateTypes;
-    private Set<String> locationTypes;
+
     private Set<String> categoryTypes;
-    private Set<String> wikiTypes;
-    private Set<String> labelTypes;
 
     private String concatToOutPath(String outFileName) {
         return outputPath + File.separator + outFileName;
@@ -44,15 +50,6 @@ public class YagoParser {
         this.geonamesCitiesFile = geonamesCitiesFile;
         this.outputPath = parserOutputPath;
 
-        dateTypes = new HashSet<>();
-        dateTypes.add("<wasBornOnDate>");
-        dateTypes.add("<diedOnDate>");
-
-        locationTypes = new HashSet<>();
-        locationTypes.add("<wasBornIn>");
-        locationTypes.add("<diedIn>");
-        locationTypes.add("<hasGender>");
-
         categoryTypes = new HashSet<>();
         categoryTypes.add("<wordnet_scientist_110560637>");
         categoryTypes.add("<wordnet_philosopher_110423589>");
@@ -62,14 +59,6 @@ public class YagoParser {
         categoryTypes.add("<wordnet_monarchist_110327824>");
         categoryTypes.add("<wordnet_poet_110444194>");
         categoryTypes.add("<wordnet_medalist_110305062>");
-        categoryTypes.add("<wordnet_city_108524735>");
-
-        wikiTypes = new HashSet<>();
-        wikiTypes.add("<hasWikipediaUrl>");
-
-        labelTypes = new HashSet<>();
-        labelTypes.add("skos:prefLabel");
-        labelTypes.add("rdfs:label");
     }
 
     // handles yagoDateFacts
@@ -77,10 +66,10 @@ public class YagoParser {
         BufferedReader br = new BufferedReader(new FileReader(yagoDateFile));
         Pattern pattern = Pattern.compile(DATE_REGEX);
 
-        File bornOnDateOutfile = new File(concatToOutPath("date_bornOnDate.tsv"));
+        File bornOnDateOutfile = new File(concatToOutPath(DATE_BORN_ON_DATE_OUT_NAME));
         PrintWriter bornOnPw = new PrintWriter(new FileWriter(bornOnDateOutfile, true), true);
 
-        File diedOnDatefile = new File(concatToOutPath("date_diedOnDate.tsv"));
+        File diedOnDatefile = new File(concatToOutPath(DATE_DIED_ON_DATE_OUT_NAME));
         PrintWriter diedOnPw = new PrintWriter(new FileWriter(diedOnDatefile, true), true);
 
         String line;
@@ -104,13 +93,13 @@ public class YagoParser {
     // handles yagoFacts
     public void parseYagoLocationFile(File yagoLocationFile) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(yagoLocationFile));
-        File genderOutfile = new File(concatToOutPath("facts_gender.tsv"));
+        File genderOutfile = new File(concatToOutPath(FACTS_GENDER_OUT_NAME));
         PrintWriter genderPw = new PrintWriter(new FileWriter(genderOutfile, true), true);
 
-        File bornInOutfile = new File(concatToOutPath("facts_born_in_location.tsv"));
+        File bornInOutfile = new File(concatToOutPath(FACTS_BORN_IN_LOCATION_OUT_NAME));
         PrintWriter bornInPw = new PrintWriter(new FileWriter(bornInOutfile, true), true);
 
-        File diedInOutfile = new File(concatToOutPath("facts_died_in_location.tsv"));
+        File diedInOutfile = new File(concatToOutPath(FACTS_DIED_IN_LOCATION_OUT_NAME));
         PrintWriter diedInPw = new PrintWriter(new FileWriter(diedInOutfile, true), true);
 
         String line;
@@ -229,12 +218,12 @@ public class YagoParser {
     // handles yagoLabels
     public void parseYagoLabelsFile(File yagoLabelsFile) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(yagoLabelsFile));
-        File labelOutfile = new File("labels_info.tsv");
-        File prefLabelOutfile = new File("pref_labels_info.tsv");
+        File labelOutfile = new File(concatToOutPath(LABELS_INFO_OUT_NAME));
+        File prefLabelOutfile = new File(concatToOutPath(PREF_LABELS_INFO_OUT_NAME));
         PrintWriter labelPw = new PrintWriter(new FileWriter(labelOutfile, true), true);
         PrintWriter prefLabelPw = new PrintWriter(new FileWriter(prefLabelOutfile, true), true);
 
-        Pattern p = Pattern.compile("\"(.*)\"@eng");
+        Pattern p = Pattern.compile(LABEL_REGEX);
         String line;
         while ((line = br.readLine()) != null) {
             String[] cols = line.trim().split("\\t");
@@ -242,7 +231,7 @@ public class YagoParser {
                 continue;
             }
             Matcher m = p.matcher(cols[3]);
-            if (m.find()) {
+            if (m.matches()) {
                 if ("skos:prefLabel".equals(cols[2])) {
                     prefLabelPw.println(cols[1] + "\t" + m.group(1));
                 } else if ("rdfs:label".equals(cols[2])) {
@@ -261,26 +250,26 @@ public class YagoParser {
             System.out.println("Terminating parser");
 //            throw new IOException("Bad input file");
         }*/
-        System.out.print("Parsing dates..");
-        parseYagoDateFile(yagoDateFile);
+        System.out.print("Parsing YAGO dates..");
+        //parseYagoDateFile(yagoDateFile);
         System.out.println("   Done");
         System.out.print("Parsing YAGO locations..");
-        parseYagoLocationFile(yagoLocationFile);
+        //parseYagoLocationFile(yagoLocationFile);
         System.out.println("   Done");
         System.out.print("Parsing YAGO categories..");
-        parseYagoCategoryFile(yagoCategoryFile);
+        //parseYagoCategoryFile(yagoCategoryFile);
         System.out.println("   Done");
         System.out.print("Parsing YAGO labels..");
         parseYagoLabelsFile(yagoLabelsFile);
         System.out.println("   Done");
         System.out.print("Parsing YAGO wikipedia info..");
-        parseYagoWikiFile(yagoWikiFile);
+        //parseYagoWikiFile(yagoWikiFile);
         System.out.println("   Done");
         System.out.print("Parsing YAGO geonames info..");
-        parseYagoGeonamesFile(yagoGeonamesFile);
+        //parseYagoGeonamesFile(yagoGeonamesFile);
         System.out.println("   Done");
         System.out.print("Parsing Geonames cities info..");
-        parseGeonamesCitiesFile(geonamesCitiesFile);
+        //parseGeonamesCitiesFile(geonamesCitiesFile);
         System.out.println("   Done");
         System.out.println("Parsing complete");
     }
@@ -293,7 +282,7 @@ public class YagoParser {
                 new File("/Users/admin/Downloads/yagoLabels.tsv"),
                 new File("/Users/admin/Downloads/yagoWikipediaInfo.tsv"),
                 new File("/Users/admin/Downloads/yagoGeonamesEntityIds.tsv"),
-                new File("/Users/admin/Downloads/cities1000.txt"), "/Users/admin/Downloads");
+                new File("/Users/admin/Downloads/cities1000.txt"), "/Users/admin/Downloads/Test");
         yagoParser.parseFiles();
     }
 
