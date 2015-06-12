8626648842140244a5be4348f03ac74af200085a
Paz Dangur <pazdangur1@gmail.com>
2015-06-12 16:04:03 +0300

    cleanup

--- src/il/ac/tau/cs/databases/atlas/connector/command/ParseFilesCommand.java
+++ src/il/ac/tau/cs/databases/atlas/connector/command/ParseFilesCommand.java
@@ -98,73 +98,6 @@ public class ParseFilesCommand extends BaseDBCommand<Boolean>{
         }
     }
 
-    private void createGeoToYagoTempTable(Connection con, String tempTableName) throws AtlasServerException {
-        String pathToParsedGeoInfo = concatToOutPath(YagoParser.GEO_INFO_OUT_NAME);
-        BufferedReader br = null;
-        PreparedStatement pstmt = null;
-        try {
-            pstmt = con.prepareStatement("CREATE TABLE IF NOT EXISTS " + tempTableName +"(" +
-                    "yago_ID VARCHAR(100) NOT NULL, " +
-                    "geo_ID INT NOT NULL, " +
-                    "PRIMARY KEY geo_ID" +
-                    ")");
-            pstmt.executeUpdate();
-            System.out.println(tempTableName + " is created");
-            pstmt = con.prepareStatement("LOAD DATA LOCAL INFILE `?` INTO TABLE `?` FIELDS TERMINATED BY `\\t`");
-            pstmt.setString(1, pathToParsedGeoInfo);
-            pstmt.setString(2, tempTableName);
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new AtlasServerException("Failed to create GeoToYago temporary table");
-        } finally {
-            safelyClose(pstmt);
-        }
-    }
-
-    private void insertLocations(Connection con, String yagoToGeoTableName) throws AtlasServerException {
-        File parsedGeoCitiesInfo = new File(concatToOutPath(YagoParser.GEO_CITIES_INFO_OUT_NAME));
-        BufferedReader br = null;
-        // Replace into so that rows will be updated or created if not exist
-        try (PreparedStatement pstmt = con.prepareStatement(
-                "REPLACE INTO `templocation`" +
-                "SET `geo_name` = ?, " +
-                "`latitude` = ?, " +
-                "`longtitude` = ?, " +
-                "`wikiURL` = ?, " +
-                "`geo_ID` = ?, " +
-                "`yago_ID` = ?")) {
-
-            //con.setAutoCommit(false); // make this into a transaction
-            br = new BufferedReader(new FileReader(parsedGeoCitiesInfo));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] cols = line.trim().split("\\t");
-                pstmt.setString(1, cols[2]);
-                pstmt.setDouble(2, Double.parseDouble(cols[3]));
-                pstmt.setDouble(3, Double.parseDouble(cols[4]));
-                pstmt.setNull(4, Types.VARCHAR);
-                pstmt.setInt(5, Integer.parseInt(cols[0]));
-                pstmt.setNull(6, Types.VARCHAR); // FIXME: this column should be nullable! After all of the insertions, the temp table and this table should be unified
-                pstmt.addBatch();
-            }
-
-            pstmt.executeBatch();
-            // TODO: unify temp table with this table
-
-
-            //con.commit();
-            System.out.println("The table `location` has been filled!");
-
-        } catch(SQLException | IOException e){
-            e.printStackTrace();
-            throw new AtlasServerException("Failed to insert locations");
-        } finally{
-            safelyClose(br);
-        }
-    }
-
     private void createCategoriesTable(Connection con) throws AtlasServerException {
         try (PreparedStatement pstmt = con
                 .prepareStatement("REPLACE INTO category(category_ID,categoryName) VALUES(?,?)")) {
@@ -304,20 +237,6 @@ public class ParseFilesCommand extends BaseDBCommand<Boolean>{
 
     private void loadDataIntoTempTable(Connection con, String tableName, String dataFilePath) throws AtlasServerException {
         System.out.println("populating temp table " + tableName);
-        /*try (PreparedStatement pstmt = con.prepareStatement(
-                "LOAD DATA LOCAL INFILE ? REPLACE INTO TABLE ? FIELDS TERMINATED BY '\\t'")) {
-            /*
-            pstmt = con.prepareStatement("LOAD DATA INFILE '" + dataFilePath +
-                    "' REPLACE INTO TABLE " + tableName + " FIELDS TERMINATED BY '\\t'");
-            pstmt.executeUpdate();
-
-            pstmt.setString(1, dataFilePath);
-            pstmt.setString(2, tableName);
-            System.out.println(pstmt.toString());
-            pstmt.executeUpdate();
-
-        }
-        */
         StringBuilder sb = new StringBuilder();
         sb.append("LOAD DATA LOCAL INFILE '" + dataFilePath + "'IGNORE INTO TABLE " + tableName + " FIELDS TERMINATED BY '\\t'");
         try (Statement stmt = con.createStatement()) {
