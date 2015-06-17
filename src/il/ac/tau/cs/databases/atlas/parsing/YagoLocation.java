9eb8e463a3bc0f48103f2a52a5758ac11fae791a
Paz Dangur <pazdangur1@gmail.com>
2015-06-17 10:45:14 +0300

    YagoLocation class

---
+++ src/il/ac/tau/cs/databases/atlas/parsing/YagoLocation.java
@@ -0,0 +1,53 @@
+package il.ac.tau.cs.databases.atlas.parsing;
+
+public class YagoLocation {
+    private long locationId;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String wikiUrl;
+
+    public long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(long locationId) {
+        this.locationId = locationId;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getWikiUrl() {
+        return wikiUrl;
+    }
+
+    public void setWikiUrl(String wikiUrl) {
+        this.wikiUrl = wikiUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isValidLocation() {
+        return ((!(latitude == 0.0 && longitude == 0.0)) && locationId != 0l && name != null);
+    }
+}
