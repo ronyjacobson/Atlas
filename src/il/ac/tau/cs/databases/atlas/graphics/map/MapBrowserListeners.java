f1e313634483f5861a2c4e5b965b1e88de69583e
Ronyjacobson <Ronyjacobson@gmail.com>
2015-06-20 17:34:16 +0300

    fixup

--- src/il/ac/tau/cs/databases/atlas/graphics/map/MapBrowserListeners.java
+++ src/il/ac/tau/cs/databases/atlas/graphics/map/MapBrowserListeners.java
@@ -363,6 +363,6 @@ public class MapBrowserListeners {
 	public static void setCategory(String cat) {
 		String exec = "setCategory(\"" + cat + "\");";
 		logger.info("Adjusting category label by executing JS:" + exec);
-		map.getBrowser().execute(exec + "hideSpinner();");
+		executeJS(exec + "hideSpinner();");
 	}
 }
