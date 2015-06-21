5adb69ee98eb71ea326a29c0c3e8d934ed81eeae
EtanKing <etan.gru@gmail.com>
2015-06-21 22:08:48 +0300

    new dist

--- dist/atlas_linux.sh
+++
@@ -1,1 +0,0 @@
-java -cp "atlas-1.0-SNAPSHOT.jar:dependency-jars/*:swt/linux/swt.jar" -Dlog4j.configuration=file:log4j.properties il.ac.tau.cs.databases.atlas.Main config.properties
---
+++ dist/atlas_linux_x64.sh
@@ -0,0 +1,1 @@
+java -cp "atlas_linux_x64.jar:dependency-jars/*:swt/linux_x64/swt.jar" -Dlog4j.configuration=file:log4j.properties il.ac.tau.cs.databases.atlas.Main config.properties
